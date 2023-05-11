70df589aab2c3f535aa5672d1b29da7447dd405b
Felipe Liberman Fuchs <felipe@almosthackers.io>
2023-05-11 09:39:39 -0300

    keep DB connection open

--- src/app.js
+++ src/app.js
@@ -4,10 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./database.db');
+
 var indexRouter = require('./routes/index');
 var classroomsRouter = require('./routes/classrooms');
 
 var app = express();
+
+// Connect to DB
+app.use(function (req, res, next) {
+    req.db = db;
+    next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,7 +24,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,19 +32,19 @@ app.use('/', indexRouter);
 app.use('/classrooms', classroomsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
