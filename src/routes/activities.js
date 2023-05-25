98b9dc2c72bb63756809a53f877b3bba9d72a6bc
lucasnn718 <lucas.nunes@sou.inteli.edu.br>
2023-05-25 18:39:53 -0300

    Create activity screen
    Responsive design for creating activities screen

--- src/routes/activities.js
+++ src/routes/activities.js
@@ -5,13 +5,14 @@ var router = express.Router();
 // GET /activities
 router.get('/', (req, res, next) => {
     const sql = 'SELECT * FROM activities'
-
+/*
     req.db.all(sql, [], (err, rows) => {
         if (err) {
             return res.status(500).json({error: err.message});
         }
         res.status(200).json(rows);
-    });
+    });*/
+    res.render('activities', {title: 'Gaba'})
 });
 
 // GET/activities/:id
