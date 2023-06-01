5b8e636a8e65ed8e207236a7b82bc1254d7384fd
bruuna-brasil <124185478+bruuna-brasil@users.noreply.github.com>
2023-06-01 08:53:39 -0300

    Adição do botão "deletar"
    O botão consegue deletar apenas uma cópia, não sei como resolver este problema ainda

--- src/public/javascripts/front_create.js
+++ src/public/javascripts/front_create.js
@@ -10,14 +10,30 @@ var codigoDuplicado; // Variável global para armazenar a cópia
 botaoClicado.addEventListener('click', funcaoDuplicar);
 deletarClicado.addEventListener('click', funcaoDeletar);
 
-// Função para duplicar a caixa de perguntas
+// Function to duplicate the code
 function funcaoDuplicar() {
   // Clonar o node/parte do html que está em codParaCopiar e colocar em codigoDuplicado
   codigoDuplicado = caixaDePerguntas.cloneNode(true);
-  codigoDuplicado.style.marginTop = '20px'; // Modifiquei mt para marginTop para corrigir o estilo
+  codigoDuplicado.style.marginTop = '30px'; // Modifiquei mt para marginTop para corrigir o estilo
+
+  // Adicionar evento de clique para o botão de deletar na cópia
+  const deletarClicadoCopia = codigoDuplicado.querySelector('#deleteQuestionButton');
+
+  // Remover o evento de clique existente antes de adicionar um novo evento
+  deletarClicadoCopia.removeEventListener('click', funcaoDeletar);
+
+  deletarClicadoCopia.addEventListener('click', funcaoDeletar);
 
   // Inserir a cópia antes da pergunta original
-  caixaDePerguntas.parentNode.insertBefore(codigoDuplicado, botoesFixos);
+  caixaDePerguntas.parentNode.insertBefore(codigoDuplicado, caixaDePerguntas);
 
+  // Adicionar novamente o evento de clique para o botão de deletar original
+  deletarClicado.addEventListener('click', funcaoDeletar);
 }
 
+function funcaoDeletar() {
+  if (codigoDuplicado) {
+    codigoDuplicado.parentNode.removeChild(codigoDuplicado);
+    codigoDuplicado = null; // Resetar a variável para null após a remoção
+  }
+}
