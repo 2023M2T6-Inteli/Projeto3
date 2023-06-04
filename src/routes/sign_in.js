929e08b3599f064123e8fa2639445bbd07382aa7
rafaelcoutinho12 <rafael.montenegro@sou.inteli.edu.br>
2023-06-04 17:00:34 -0300

    create new user

--- src/routes/sign_in.js
+++ src/routes/sign_in.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
+const crypto = require('crypto-js');
 
 const DBPATH = './database.db';
 
@@ -12,46 +13,69 @@ router.get('/', function(req, res, next) {
   res.render('sign_in', { title: 'Gaba' });
 });
 
-/*Login*/ 
-router.post('/sign_in',urlencodedParser, (req,res)=>{
+/* Login */
+router.post('/sign_in', urlencodedParser, (req, res) => {
   var name;
-  //Garantir que a requisição tem código inicial correto
+  // Ensure the request has the correct initial code
   res.statusCode = 200;
-  //Define o cabeçalho da requisição
-  res.setHeader('Acess-Control-Allow-Origin','*');
-  //Inicializa o banco de dados
+  // Set the request header
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Initialize the database
   var db = new sqlite3.Database(DBPATH);
-  //Varíavel para a definição da sentença SQl
+  // Variable to define the SQL statement
   var sql = 'SELECT * FROM users WHERE EMAIL = "' + req.body.email + '" AND encrypted_password = "' + req.body.encrypted_password + '"';
-  var sqlpasta = `SELECT * FROM users WHERE users.id = "`
+  var sqlpasta = `SELECT * FROM users WHERE users.id = "`;
   console.log(sql);
-  db.all(sql, [], ( err,rows) => {
-     if (err) {
-         console.error(err);
-         res.status(500).json({ error: 'An error occurred' });
-       } else if (rows.length === 0) {
-         res.status(401).json({ error: 'Invalid username or email' });
-       } else if (rows[0].encrypted_password !== req.body.encrypted_password) {
-         console.error(err);
-         console.log(rows);
-         res.status(401).json({ error: 'Invalid password' });
-       } else {
-          name = rows[0].first_name;
-          sqlpasta += rows[0].id + `";`;
-          console.log(sqlpasta);
-          db.all(sqlpasta,[],(err,rows)=>{
-              if(err){
-                  console.log(err);
-              }
-              console.log(rows);
-              if(rows !== null){
-              res.render("menu");}
-              {
-                  res.render("menu");
-              }
-          })
-      }
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'An error occurred' });
+    } else if (rows.length === 0) {
+      res.status(401).json({ error: 'Invalid username or email' });
+    } else if (rows[0].encrypted_password !== req.body.encrypted_password) {
+      console.error(err);
+      console.log(rows);
+      res.status(401).json({ error: 'Invalid password' });
+    } else {
+      name = rows[0].first_name;
+      sqlpasta += rows[0].id + `";`;
+      console.log(sqlpasta);
+      db.all(sqlpasta, [], (err, rows) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(rows);
+        if (rows !== null) {
+          res.render("menu");
+        } else {
+          res.render("menu");
+        }
+      });
+    }
   });
-})
+});
+
+// Create a new user
+router.post('/sign_up', urlencodedParser, (req, res) => {
+  // Ensure the request has the correct initial code
+  res.statusCode = 200;
+  // Set the request header
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Initialize the database
+  var db = new sqlite3.Database(DBPATH);
+  // Variable to define the SQL statement
+  var sql = 'INSERT INTO users (id, first_name, last_name, email, encrypted_password) VALUES(null,"' + req.body.first_name + '","' + req.body.last_name + '","' + req.body.email + '","' + req.body.encrypted_password + '");';
+  console.log(sql);
+  db.run(sql, [], err => {
+    if (err) {
+      console.log("Error inserting data");
+      // Log the error to the console to prevent a general crash
+      throw err;
+    }
+  });
+  db.close();
+  res.render("tutorial");
+  res.end();
+});
 
 module.exports = router;
