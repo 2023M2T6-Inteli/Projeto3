99f72ef749111b9f922768bb40b051f8335b76d2
rafaelcoutinho12 <rafael.montenegro@sou.inteli.edu.br>
2023-06-05 14:33:08 -0300

    fix variable

--- src/routes/sign_in.js
+++ src/routes/sign_in.js
@@ -42,7 +42,7 @@ router.post('/sign_in', urlencodedParser, (req, res) => {
         res.status(401).send('<script>alert("Invalid password"); window.location.href = "/sign_in";</script>');
       } else {
         req.session.user_id = rows[0].id;
-        req.session.name = rows[0].first_name;
+        req.session.name = rows[1].first_name;
         req.session.auth = true
         name = rows[0].first_name;
         sqlpasta += rows[0].id + `";`;
