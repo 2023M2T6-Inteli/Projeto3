1dedfedc3fca300f6601768b0c7d19c76868358b
Felipe Liberman Fuchs <felipe@almosthackers.io>
2023-06-12 15:23:09 -0300

    Make new question button work

--- src/public/javascripts/front_create.js
+++ src/public/javascripts/front_create.js
@@ -1,2 +1,17 @@
-var editor1 = new RichTextEditor("#div_editor_1");
+let editors = [];
+
+function createRichTextEditor() {
+    let id = editors.length.toString();
+
+    let editor = document.createElement("div");
+    editor.setAttribute("id", "div_editor_" + id);
+    editor.setAttribute("class", "mb-4 p-4 rounded-2xl");
+    document.getElementById("editors").appendChild(editor);
+
+    editors.push(new RichTextEditor("#div_editor_" + id));
+}
+
+createRichTextEditor();
+document.getElementById("btn_add_editor").addEventListener("click", createRichTextEditor);
+
 //editor1.setHTMLCode("Use inline HTML or setHTMLCode to init the default content.");
