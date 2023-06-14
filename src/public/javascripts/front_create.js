384f58ae7426498ed836c8fb5dbeb778fc6789af
Felipe Liberman Fuchs <felipe@almosthackers.io>
2023-06-14 13:22:20 -0300

    connect acitivty creation with Backend

--- src/public/javascripts/front_create.js
+++ src/public/javascripts/front_create.js
@@ -11,20 +11,61 @@ function createRichTextEditor() {
     editors.push(new RichTextEditor("#div_editor_" + id));
 }
 
-function saveActivity() {
-    let activityId = createActivity(title);
+async function saveActivity() {
+    let title = document.getElementById("input_title").value;
+    let activityId = await createActivity(title);
 
     editors.forEach(editor => {
-        createQuestion(activityId)
+        createQuestion(activityId, editor)
     });
 }
 
-function createQuestion(activityId) {
-    alert("Created question with Activity ID: " + activityId);
+async function createQuestion(activityId, editor) {
+    const response = await fetch("http://127.0.0.1:3000/questions/api", {
+        method: "POST",
+        body: JSON.stringify({
+            // TODO: Remove static params
+            activity_id: activityId,
+            criterium_id: 1,
+            max_grade_percent: 50,
+            content: editor.getHTMLCode()
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    });
+
+    if (response.ok) {
+        // Show success modal
+    }
+    else {
+        alert("Erro!")
+        throw new Error("HTTP-Error: " + response.status);
+    }
 }
 
-function createActivity(title) {
-    return 1
+async function createActivity(title) {
+    const response = await fetch("http://127.0.0.1:3000/activities/api", {
+        method: "POST",
+        body: JSON.stringify({
+            // TODO: Remove these IDs
+            user_id: 1,
+            classroom_id: 1,
+            title: title
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    });
+
+    if (response.ok) {
+        const json = await response.json();
+        return json.id;
+    }
+    else {
+        alert("Erro!")
+        throw new Error("HTTP-Error: " + response.status);
+    }
 }
 
 createRichTextEditor();
