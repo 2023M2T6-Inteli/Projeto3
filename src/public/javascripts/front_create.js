d4c1116b4274a2146d856ac1193c358b5e4ebe14
Felipe Liberman Fuchs <felipe@almosthackers.io>
2023-06-15 09:48:54 -0300

    Fetch data and add to Criteria DropDown

--- src/public/javascripts/front_create.js
+++ src/public/javascripts/front_create.js
@@ -5,7 +5,26 @@ document.getElementById("btn_add_editor").addEventListener("click", createRichTe
 document.getElementById("btn_save").addEventListener("click", saveActivity);
 document.getElementById("btn_home_page").addEventListener("click", () => { window.location.href = "/activities" });
 
-function criteriaDropdown(id) {
+
+/**
+ * Fetched all criteria from the database
+ * @return {[Array]}     Array with Criteria objects
+ */
+async function criteria() {
+    let response = await fetch("http://127.0.0.1:3000/criteria", {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    });
+
+    return await response.json();
+}
+
+/**
+* Builds the criteria dropdown
+*/
+async function criteriaDropdown(id) {
     let criteriaDropdown = document.createElement("select");
     criteriaDropdown.setAttribute("id", "select_criteria_" + id);
     criteriaDropdown.setAttribute("class", "mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500");
@@ -16,23 +35,26 @@ function criteriaDropdown(id) {
     placeholder.setAttribute("selected", "true");
     criteriaDropdown.appendChild(placeholder);
 
-    for (var i = 0; i <= 5; i++) {
+    (await criteria()).forEach(criterium => {
         var opt = document.createElement('option');
-        opt.value = i;
-        opt.innerHTML = i;
+        opt.value = criterium.id;
+        opt.innerHTML = criterium.synthesis;
         criteriaDropdown.appendChild(opt);
-    }
+    });
 
     return criteriaDropdown
 }
 
-function createRichTextEditor() {
+/**
+* Creates a new RichTextEditor
+*/
+async function createRichTextEditor() {
     let id = editors.length.toString();
 
     let editorContainer = document.createElement("div");
     editorContainer.setAttribute("class", "mb-4 p-4 rounded-2xl bg-white");
 
-    editorContainer.appendChild(criteriaDropdown(id));
+    editorContainer.appendChild(await criteriaDropdown(id));
 
     let editor = document.createElement("div");
     editor.setAttribute("id", "div_editor_" + id);
@@ -42,6 +64,9 @@ function createRichTextEditor() {
     editors.push(new RichTextEditor("#div_editor_" + id));
 }
 
+/**
+* Saves everything on screen
+*/
 async function saveActivity() {
     let title = document.getElementById("input_title").value;
     let activityId = await createActivity(title);
@@ -53,6 +78,9 @@ async function saveActivity() {
     document.getElementById("saved_modal").classList.toggle("hidden");
 }
 
+/**
+* Creates a question on the DB
+*/
 async function createQuestion(activityId, editor, index) {
     const response = await fetch("http://127.0.0.1:3000/questions/api", {
         method: "POST",
@@ -77,6 +105,9 @@ async function createQuestion(activityId, editor, index) {
     }
 }
 
+/**
+* Creates an activity on the DB
+*/
 async function createActivity(title) {
     const response = await fetch("http://127.0.0.1:3000/activities/api", {
         method: "POST",
