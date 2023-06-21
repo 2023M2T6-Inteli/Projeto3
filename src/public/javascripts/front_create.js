a19a39e6e3d627ae54b32a45a8ed441362f6f1fe
Felipe Liberman Fuchs <felipe@almosthackers.io>
2023-06-21 18:52:07 -0400

    refactor some parts of front_create.js

--- src/public/javascripts/front_create.js
+++ src/public/javascripts/front_create.js
@@ -9,8 +9,8 @@ document.getElementById("btn_print").addEventListener("click", saveAndPrint);
 
 
 /**
- * Fetched all criteria from the database
- * @return {[Array]}     Array with Criteria objects
+ * Pega critérios da Base de Dados
+ * @return {[Array]}     Array com critérios da BNCC
  */
 async function criteria() {
     let response = await fetch("http://127.0.0.1:3000/criteria", {
@@ -24,7 +24,7 @@ async function criteria() {
 }
 
 /**
-* Builds the criteria dropdown
+* Dropwdown com os critérios da BNCC
 */
 async function criteriaDropdown(id) {
     let criteriaDropdown = document.createElement("select");
@@ -48,7 +48,34 @@ async function criteriaDropdown(id) {
 }
 
 /**
-* Creates a new RichTextEditor
+ * Caixa de texto para inserir a nota máxima da questão
+ */
+function gradeInput(id) {
+    let grade_box = document.createElement("input");
+    grade_box.setAttribute("id", "input_grade_" + id);
+    grade_box.setAttribute("placeholder", "Máximo de Pontos");
+    grade_box.setAttribute("type", "number");
+    grade_box.setAttribute("min", "0");
+    grade_box.setAttribute("class", "mr-4 w-1/3 bg-gray-50 dark:bg-[#1F1F1F] border border-gray-300 text-[#16afb8] text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500");
+
+    return grade_box
+}
+
+/**
+ * Botão para remover a questão
+ */
+function removeQuestionButton(id) {
+    let remove_editor = document.createElement("button");
+    remove_editor.setAttribute("id", "button_remove_editor_" + id);
+    remove_editor.setAttribute("class", "mr-auto font-medium shadow rounded-lg text-sm px-4 text-center bg-[#F0F0F0] sm:bg-[#16afb8] text-[#16AFB8] sm:text-[#F0F0F0] hover:bg-[#D9D9D9] sm:hover:bg-[#369398] sm:px-5 py-3.5");
+    remove_editor.innerHTML = "Remover questão";
+    remove_editor.addEventListener("click", () => { editors[id].deleted = true; document.getElementById("div_editor_container_" + id).remove() });
+
+    return remove_editor
+}
+
+/**
+* Cria um editor de texto
 */
 async function createRichTextEditor() {
     let id = editors.length.toString();
@@ -56,7 +83,6 @@ async function createRichTextEditor() {
     let editorContainer = document.createElement("div");
     editorContainer.setAttribute("id", "div_editor_container_" + id);
     editorContainer.setAttribute("class", "mb-4 p-4 rounded-2xl bg-white dark:bg-[#1F1F1F]");
-
     editorContainer.appendChild(await criteriaDropdown(id));
 
     // Editor de Texto
@@ -69,30 +95,18 @@ async function createRichTextEditor() {
     toolbar.setAttribute("class", "flex mt-4 ");
 
     // Caixa com valor da questão
-    let grade_box = document.createElement("input");
-    grade_box.setAttribute("id", "input_grade_" + id);
-    grade_box.setAttribute("placeholder", "Máximo de Pontos");
-    grade_box.setAttribute("type", "number");
-    grade_box.setAttribute("min", "0");
-    grade_box.setAttribute("class", "mr-4 w-1/3 bg-gray-50 dark:bg-[#1F1F1F] border border-gray-300 text-[#16afb8] text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500");
-    toolbar.appendChild(grade_box);
+    toolbar.appendChild(gradeInput(id));
 
     // Botão de remover a questão
-    let remove_editor = document.createElement("button");
-    remove_editor.setAttribute("id", "button_remove_editor_" + id);
-    remove_editor.setAttribute("class", "mr-auto font-medium shadow rounded-lg text-sm px-4 text-center bg-[#F0F0F0] sm:bg-[#16afb8] text-[#16AFB8] sm:text-[#F0F0F0] hover:bg-[#D9D9D9] sm:hover:bg-[#369398] sm:px-5 py-3.5");
-    remove_editor.innerHTML = "Remover questão";
-    remove_editor.addEventListener("click", () => {editors[id].deleted = true; document.getElementById("div_editor_container_" + id).remove()});
-    toolbar.appendChild(remove_editor);
+    toolbar.appendChild(removeQuestionButton(id));
 
     editorContainer.appendChild(toolbar);
-
     document.getElementById("editors").appendChild(editorContainer);
-    editors.push({deleted: false, editor: new RichTextEditor("div_editor_" + id)});
+    editors.push({ deleted: false, editor: new RichTextEditor("div_editor_" + id) });
 }
 
 /**
-* Saves everything on screen
+* Salva a atividade no banco de dados
 */
 async function saveActivity() {
     let title = document.getElementById("input_title").value;
@@ -106,13 +120,12 @@ async function saveActivity() {
 }
 
 /**
-* Creates a question on the DB
+* Cria uma questão no banco de dados
 */
 async function createQuestion(activityId, editor, index) {
     const response = await fetch("http://127.0.0.1:3000/questions/api", {
         method: "POST",
         body: JSON.stringify({
-            // TODO: Remove static params
             activity_id: activityId,
             criterium_id: document.getElementById("select_criteria_" + index).value,
             max_grade_percent: document.getElementById("input_grade_" + index).value,
@@ -133,7 +146,7 @@ async function createQuestion(activityId, editor, index) {
 }
 
 /**
-* Creates an activity on the DB
+* Cria uma atividade no banco de dados
 */
 async function createActivity(title) {
     const response = await fetch("http://127.0.0.1:3000/activities/api", {
@@ -160,7 +173,7 @@ async function createActivity(title) {
 }
 
 /**
-* Saves the activity and redirects to print page
+* Salva a atividade e redireciona para a página de impressão
 */
 function saveAndPrint() {
     // Espera salvar a atividade e redireciona para a página de impressão
