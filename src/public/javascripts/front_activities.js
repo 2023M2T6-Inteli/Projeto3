9ed17b650b888903a2111a0abbceca35a88ae3ac
Felipe Liberman Fuchs <felipe@almosthackers.io>
2023-06-21 09:27:41 -0400

    allow user to view activities

--- src/public/javascripts/front_activities.js
+++ src/public/javascripts/front_activities.js
@@ -1,10 +1,12 @@
-async function activities(){
+// Drop-down de atividades
+async function activities() {
     const response = await fetch('/activities/api');
     const data = await response.json();
     return data;
 }
 
 let activitiesSelect = document.getElementById("activities_select");
+let questions_div = document.getElementById("questions_div");
 
 activities().then(data => {
     data.forEach(activity => {
@@ -14,3 +16,25 @@ activities().then(data => {
         activitiesSelect.appendChild(option);
     });
 });
+
+// Carregamento das Perguntas
+async function fetchQuestion() {
+    const response = await fetch('/activities/api/' + activitiesSelect.value);
+    return await response.json();
+}
+
+async function loadActivity() {
+    questions_div.innerHTML = ""; // Limpa as perguntas
+
+    // Exibe as perguntas na tela
+    const questions = await fetchQuestion()
+    for (const question of questions) {
+        let question_ui = document.createElement("div");
+        question_ui.setAttribute("id", "div_question_" + question.id);
+        question_ui.classList.add("mb-4", "p-4", "rounded-2xl", "bg-white");
+        question_ui.innerHTML = question.content;
+        questions_div.appendChild(question_ui);
+    }
+}
+
+document.getElementById("btn_view").addEventListener("click", loadActivity);
--- src/routes/activities.js
+++ src/routes/activities.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET exam page. */
-router.get('/', function(req, res, next) {
-  if(req.session.auth){
+router.get('/', function (req, res, next) {
+  if (req.session.auth) {
     res.render('activities', { title: 'Gaba' });
-    }
-    else{
+  }
+  else {
     res.redirect('/');
   }
 });
@@ -16,38 +16,35 @@ router.get('/api', (req, res, next) => {
   const sql = 'SELECT * FROM activities'
 
   req.db.all(sql, [], (err, rows) => {
-      if (err) {
-          return res.status(500).json({error: err.message});
-      }
-      res.status(200).json(rows);
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.status(200).json(rows);
   });
 });
 
 // GET/activities/api/:id
 router.get('/api/:id', (req, res, next) => {
-  const sql = 'SELECT * FROM activities WHERE id = ?'
+  const sql = 'SELECT * FROM questions WHERE activity_id = ?'
 
-  req.db.get(sql, [req.params.id], (err, row) => {
-      if (err) {
-          return res.status(400).json({error: err.message});
-      }
-
-      if (row === undefined) return res.sendStatus(404);
-
-      res.status(200).json(row);
+  req.db.all(sql, [req.params.id], (err, rows) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.status(200).json(rows);
   });
 });
 
 // POST /api/activities
 router.post('/api/', (req, res, next) => {
-  const {name, user_id, classroom_id} = req.body;
+  const { name, user_id, classroom_id } = req.body;
   const sql = 'INSERT INTO activities(name, user_id, classroom_id) VALUES (?, ?, ?)'
 
   req.db.run(sql, [name, user_id, classroom_id], function (err) {
-      if (err) {
-          return res.status(400).json({error: err.message});
-      }
-      res.status(201).json({id: this.lastID});
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+    res.status(201).json({ id: this.lastID });
   });
 });
 
